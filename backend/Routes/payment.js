e91c87bfcf0278a97970d30729bcd781cbae64fd
The Jefferson <thejefferson29@gmail.com>
2025-03-10 13:35:32 +0100

    try commit

--- backend/Routes/payment.js
+++ backend/Routes/payment.js
@@ -7,6 +7,6 @@ const router = express.Router();
 router.post("/payment", processPayment);
 
 // Webhook route (Tranzak will send events here)
-router.post("/process/tranzak-webhook", webhookHandler);
+router.post("/tranzak-webhook", webhookHandler);
 
 module.exports = router;
